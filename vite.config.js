9c05a353523bd4fbe797f2d984d53c44b51f86c7
Hortice Adognibo <hortice@example.com>
2026-05-16 17:20:32 +0100

    fix: proxy API via vite

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: 'globalThis',
-  },
+  define: { global: 'globalThis' },
   server: {
-    port: 3000
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://76.13.37.17:8085',
+        changeOrigin: true,
+        secure: false
+      }
+    }
   }
 })
